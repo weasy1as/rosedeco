60c6bb18b43c21fec43d7df1407545bdf61ea256
weasy1as <habdu21@student.sdu.dk>
2025-10-08 14:26:22 +0200

    created ui components and navbar

---
+++ src/app/components/NavBar.tsx
@@ -0,0 +1,56 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div className="flex items-center justify-between p-4 bg-gray-100 shadow-md">
+      <div className="flex items-center space-x-4">
+        <Image
+          src="/logo.jpg"
+          alt="Logo"
+          width={50}
+          height={50}
+          className="rounded-full"
+        />
+        <h2 className="text-2xl font-bold tracking-tight">
+          The Rose Decorations
+        </h2>
+      </div>
+      <div className="flex items-center text-xl font-sans space-x-4 text-gray-600 font-medium">
+        <Link className="hover:text-[#D4AF37] transition-colors" href="/">
+          Home
+        </Link>
+        <Link className="hover:text-[#D4AF37] transition-colors" href="/about">
+          Packages
+        </Link>
+        <Link
+          className="hover:text-[#D4AF37] transition-colors"
+          href="/gallery"
+        >
+          Gallery
+        </Link>
+        <Link className="hover:text-[#D4AF37] transition-colors" href="/about">
+          About
+        </Link>
+        <Link
+          className="hover:text-[#D4AF37] transition-colors"
+          href="/contact"
+        >
+          Contact
+        </Link>
+      </div>
+      <div>
+        <Button
+          size={"lg"}
+          className="bg-blush-500 text-md font-bold hover:bg-blush-600"
+        >
+          Book now
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
